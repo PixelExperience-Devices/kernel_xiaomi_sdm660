9b7864c4c757ca9fb0a05cf5a5b494b816ffe2ef
Julian Liu <wlootlxt123@gmail.com>
2019-10-25 05:04:58 +0800

    cpufreq: schedutil: tune default rate limit from Pixel

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -813,9 +813,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->down_rate_limit_us = policy->down_transition_delay_us;
 	} else {
 		unsigned int lat;
-
-		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 20000;
+                tunables->up_rate_limit_us = LATENCY_MULTIPLIER / 2;
+                tunables->down_rate_limit_us = LATENCY_MULTIPLIER * 20;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
                         tunables->up_rate_limit_us *= lat;
