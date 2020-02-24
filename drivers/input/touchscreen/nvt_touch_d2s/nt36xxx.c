97ae4ad95b1a03a5a3f103628e9bbd66cb5c5a45
dianlujitao <dianlujitao@lineageos.org>
2020-02-24 20:45:09 +0100

    nvt_touch_d2s: Implement double_tap node
    Change-Id: I66b0497d0a7f87cff686254153eb3e5e1f0b889b

--- drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_d2s/nt36xxx.c
@@ -43,6 +43,10 @@ extern void Boot_Update_Firmware(struct work_struct *work);
 #endif
 static int fb_notifier_callback(struct notifier_block *self, unsigned long event, void *data);
 
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
+
 static const struct nvt_ts_mem_map NT36772_memory_map = {
 	.EVENT_BUF_ADDR           = 0x11E00,
 	.RAW_PIPE0_ADDR           = 0x10000,
@@ -239,6 +243,34 @@ int nvt_gesture_switch(struct input_dev *dev, unsigned int type, unsigned int co
 	return 0;
 }
 
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+			       struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", enable_gesture_mode);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+				struct kobj_attribute *attr, const char *buf,
+				size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	enable_gesture_mode = !!val;
+	return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+	.show = double_tap_show,
+	.store = double_tap_store
+};
+#endif
+#endif
+
 void nvt_ts_wakeup_gesture_report(uint8_t gesture_id, uint8_t *data)
 {
 	uint32_t keycode = 0;
@@ -301,7 +333,6 @@ void nvt_ts_wakeup_gesture_report(uint8_t gesture_id, uint8_t *data)
 		input_sync(ts->input_dev);
 	}
 }
-#endif
 
 int32_t CTP_I2C_READ(struct i2c_client *client, uint16_t address, uint8_t *buf, uint16_t len)
 {
@@ -766,6 +797,9 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++)
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	wake_lock_init(&gestrue_wakelock, WAKE_LOCK_SUSPEND, "poll-wake-lock");
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	ret = tp_common_set_double_tap_ops(&double_tap_ops);
+#endif
 #endif
 
 	sprintf(ts->phys, "input/ts");
