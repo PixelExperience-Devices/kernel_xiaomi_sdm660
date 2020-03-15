4b52e72afa18bf23752725414b9bb6c300858de6
Dušan Uverić <dusan.uveric9@gmail.com>
2020-03-15 02:15:25 +0100

    clk-cpu-osm: hardcode speedbin value
    * This way every sdm660 device will run uncapped with 1.8/2.2GHz freq steps
    * Discovered attempting to overclock with speedbin 0
    
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -3094,9 +3094,8 @@ static unsigned long perfcl_boot_rate = 1747200000;
 static int clk_cpu_osm_driver_probe(struct platform_device *pdev)
 {
 	int rc = 0, cpu, i;
-	int speedbin = 0, pvs_ver = 0;
+	int speedbin = 1, pvs_ver = 0;
 	bool is_sdm630 = 0;
-	u32 pte_efuse;
 	int num_clks = ARRAY_SIZE(osm_qcom_clk_hws);
 	struct clk *clk;
 	struct clk *ext_xo_clk, *ext_hmss_gpll0_clk_src;
@@ -3151,9 +3150,6 @@ static int clk_cpu_osm_driver_probe(struct platform_device *pdev)
 
 	if (pwrcl_clk.vbases[EFUSE_BASE]) {
 		/* Multiple speed-bins are supported */
-		pte_efuse = readl_relaxed(pwrcl_clk.vbases[EFUSE_BASE]);
-		speedbin = ((pte_efuse >> PWRCL_EFUSE_SHIFT) &
-						    PWRCL_EFUSE_MASK);
 		snprintf(pwrclspeedbinstr, ARRAY_SIZE(pwrclspeedbinstr),
 			 "qcom,pwrcl-speedbin%d-v%d", speedbin, pvs_ver);
 	}
@@ -3170,9 +3166,6 @@ static int clk_cpu_osm_driver_probe(struct platform_device *pdev)
 
 	if (perfcl_clk.vbases[EFUSE_BASE]) {
 		/* Multiple speed-bins are supported */
-		pte_efuse = readl_relaxed(perfcl_clk.vbases[EFUSE_BASE]);
-		speedbin = ((pte_efuse >> PERFCL_EFUSE_SHIFT) &
-							PERFCL_EFUSE_MASK);
 		snprintf(perfclspeedbinstr, ARRAY_SIZE(perfclspeedbinstr),
 			 "qcom,perfcl-speedbin%d-v%d", speedbin, pvs_ver);
 	}
